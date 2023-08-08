0958eab4fa2d75e5009b6e7fd0e9b966d342fbe5
unknown <charleseduardofaria@gmail.com>
2023-08-08 18:01:53 -0300

    Criando classe de departamento

--- src/br/com/charlesedu/app/App.java
+++ src/br/com/charlesedu/app/App.java
@@ -1,6 +1,11 @@
 package br.com.charlesedu.app;
+
+import br.com.charlesedu.model.entities.Department;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Department obj = new Department(1, "Books");
+
+        System.out.println(obj);
     }
 }

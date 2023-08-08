760e3706b0f982c035bed827090980807de23cb5
unknown <charleseduardofaria@gmail.com>
2023-08-08 18:24:50 -0300

    Criando entitadade vendendor

--- src/br/com/charlesedu/app/App.java
+++ src/br/com/charlesedu/app/App.java
@@ -1,11 +1,16 @@
 package br.com.charlesedu.app;
 
+import java.util.Date;
+
 import br.com.charlesedu.model.entities.Department;
+import br.com.charlesedu.model.entities.Seller;
 
 public class App {
     public static void main(String[] args) throws Exception {
         Department obj = new Department(1, "Books");
 
-        System.out.println(obj);
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+
+        System.out.println(seller);
     }
 }

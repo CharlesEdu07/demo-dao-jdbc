80db978ab64d0c0d9047bed0dd8ae9c43aa8b8d1
unknown <charleseduardofaria@gmail.com>
2023-08-14 10:40:33 -0300

    Implementando findById

--- src/br/com/charlesedu/app/App.java
+++ src/br/com/charlesedu/app/App.java
@@ -1,19 +1,15 @@
 package br.com.charlesedu.app;
-
-import java.util.Date;
-
 import br.com.charlesedu.model.dao.DaoFactory;
 import br.com.charlesedu.model.dao.SellerDao;
-import br.com.charlesedu.model.entities.Department;
 import br.com.charlesedu.model.entities.Seller;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Department obj = new Department(1, "Books");
-
-        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
-
         SellerDao sellerDao = DaoFactory.createSellerDao();
+
+        System.out.println("=== TEST 1: seller findById ===");
+
+        Seller seller = sellerDao.findById(3);
 
         System.out.println(seller);
     }
--- src/br/com/charlesedu/model/dao/DaoFactory.java
+++ src/br/com/charlesedu/model/dao/DaoFactory.java
@@ -1,9 +1,10 @@
 package br.com.charlesedu.model.dao;
 
+import br.com.charlesedu.db.DB;
 import br.com.charlesedu.model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
     public static SellerDao createSellerDao() {
-        return new SellerDaoJDBC();
+        return new SellerDaoJDBC(DB.getConnection());
     }
 }

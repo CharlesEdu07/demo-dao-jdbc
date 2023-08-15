d2489b02327fe59807e01b75fd3834b5450eaf31
unknown <charleseduardofaria@gmail.com>
2023-08-14 21:38:52 -0300

    Deletando do banco de dados

--- src/br/com/charlesedu/app/App.java
+++ src/br/com/charlesedu/app/App.java
@@ -1,6 +1,7 @@
 package br.com.charlesedu.app;
 
 import java.util.List;
+import java.util.Scanner;
 
 import br.com.charlesedu.model.dao.DaoFactory;
 import br.com.charlesedu.model.dao.SellerDao;
@@ -9,6 +10,8 @@ import br.com.charlesedu.model.entities.Seller;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println("=== TEST 1: seller findById ===");
@@ -33,14 +36,14 @@ public class App {
             System.out.println(obj);
         }
 
-        System.out.println("\n=== TEST 4: seller insert ===");
+        // System.out.println("\n=== TEST 4: seller insert ===");
 
-        Seller newSeller = new Seller(null, "Reylison", "reyli@gmail.com", new java.util.Date(), 4000.0,
-                new Department(3, null));
+        // Seller newSeller = new Seller(null, "Reylison", "reyli@gmail.com", new java.util.Date(), 4000.0,
+        //         new Department(3, null));
 
-        sellerDao.insert(newSeller);
+        // sellerDao.insert(newSeller);
 
-        System.out.println("Inserted! New id = " + newSeller.getId());
+        // System.out.println("Inserted! New id = " + newSeller.getId());
 
         System.out.println("\n=== TEST 5: seller update ===");
 
@@ -52,5 +55,16 @@ public class App {
         sellerDao.update(seller);
 
         System.out.println("Update completed");
+
+        System.out.println("\n=== TEST 6: seller delete ===");
+
+        System.out.print("\nEnter id for delete test: ");
+        int id = sc.nextInt();
+
+        sellerDao.deleteById(id);
+
+        System.out.println("Delete completed");
+        
+        sc.close();
     }
 }
